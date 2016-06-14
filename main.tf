f81a4a553a327a47904a98f37fdff8be5b3c7c5b
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-14 14:24:53 -0700

    removed provider_region var and use env aws defaults

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  region = "${var.provider_region}"
 }
 
 module "env" {
   source = "../module-aws-env"
 
-  provider_region = "${var.provider_region}"
   bucket_remote_state = "${var.bucket_remote_state}"
   context_org = "${var.context_org}"
   context_env = "${var.context_env}"
